f63ef9ba1c476aa3149d290eb037a3727b12edcd
Max1041065 <1041065@mborijnland.nl>
2026-04-05 14:47:14 +0200

    footer.php bijgewerkt voor social media links

--- footer.php
+++ footer.php
@@ -1,10 +1,33 @@
 <div class="footer-inner">
-      <img class="footer-logo" src="img/logo.png" alt="Vlam en Vlees">
+
+  <div class="footer-left">
+    <img class="footer-logo" src="img/logo.png" alt="Vlam en Vlees">
+
+    <div class="footer-text">
       <p class="footer-tagline">Meesters in vuur &amp; smaak — Zoetermeer</p>
+
       <nav class="footer-nav">
-        <a href="menu.php">Menu</a>
-        <a href="reserveer.php">Reserveren</a>
-        <a href="openingstijden.php">Contact</a>
+        <a href="menu.html">Menu</a>
+        <a href="reserveer.html">Reserveren</a>
+        <a href="openingstijden.html">Contact</a>
       </nav>
+
       <p class="footer-copy">&copy; 2026 Vlam &amp; Vlees. Alle rechten voorbehouden.</p>
     </div>
+  </div>
+
+    <section class="footer-social">
+    <a href="#" aria-label="YouTube">
+      <img src="img/youtube.png" alt="Logo van YouTube">
+    </a>
+    <a href="#" aria-label="Instagram">
+      <img src="img/instagram.png" alt="Logo van Instagram">
+    </a>
+    <a href="#" aria-label="Facebook">
+      <img src="img/facebook.png" alt="Logo van Facebook">
+    </a>
+    <a href="#" aria-label="Twitter">
+      <img src="img/twitter.png" alt="Logo van X/Twitter">
+    </a>
+    </section>
+</div>
